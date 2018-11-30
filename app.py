719fe2ae6a1624e684abcfd829156b64abd47988
Allen <970414076@qq.com>
2018-11-30 15:45:53 +0800

    增加app.py文件

---
+++ app.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+"""
+@author: LEH
+@file: app.py
+@time: 2018/11/30 15:42
+"""
+print('hello')
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
